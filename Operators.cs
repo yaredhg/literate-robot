0fd434b1291aee82325a682d5f04461e29e5ce2f
yaredhg <60713855+yaredhg@users.noreply.github.com>
2020-02-05 19:11:58 -0800

    Add files via upload

---
+++ Operators.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace ArtimeticExpressionCaclulator
+{
+    public class Operators
+    {
+        public char Value { get; set; }
+        public int Precedence { get; set; }
+
+        public Operators(char value)
+        {
+            Value = value;
+            Precedence = GetPreference(value);
+        }
+        /// <summary>
+        /// This will give Division and multiplication higher precedence
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private int GetPreference(char value)
+        {
+            int result = 1;
+            switch (value)
+            {                
+                case '*':
+                case '/':
+                    result = 2;
+                    break;
+
+            }
+            return result;
+        }
+    }
+}
